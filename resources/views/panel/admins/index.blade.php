d5792940c1bc0127817d53f1f707c3c2caaef1e0
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2023-06-20 23:02:25 +0330

    changing label

--- resources/views/panel/admins/index.blade.php
+++ resources/views/panel/admins/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="breadcrumb-wrapper">
-        <h1>مدیریت مدیران</h1>
+        <h1>مدیریت کاربران</h1>
 
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb p-0">
