358cd22ab281b9ee7ea18f82b23c1658d4503df7
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2023-07-22 01:31:19 +0330

    adding cluster seeder and factory

---
+++ database/factories/ClusterFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Content;
+use App\Models\File;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cluster>
+ */
+class ClusterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content_id' => Content::query()->inRandomOrder()->first()->id,
+            'name' => fake()->title(),
+            'description' => fake()->paragraph(3),
+            'cover_id' => File::query()->inRandomOrder()->first()->id
+        ];
+    }
+}
---
+++ database/seeders/ClusterSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cluster;
+use Illuminate\Database\Seeder;
+
+class ClusterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cluster::factory()
+            ->count(15)
+            ->create();
+    }
+}
