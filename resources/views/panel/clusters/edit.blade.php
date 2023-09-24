2aceb9119bfc862cef36827b0b4a55a3e9c2abc9
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2023-09-24 20:00:24 +0330

    video style optimization

--- resources/views/panel/clusters/edit.blade.php
+++ resources/views/panel/clusters/edit.blade.php
@@ -129,12 +129,15 @@
 
                                 <div class="col-6 justify-content-center pt-4">
                                     <input class="step_cover_id" value="{{$step->cover_id}}" hidden>
-                                    <img src="{{$step->cover_image}}" width="100%" style="max-height: 300px; width: auto">
+                                    <img src="{{$step->cover_image}}" style="max-height: 300px; width: auto;">
                                 </div>
 
                                 <div class="col-6 justify-content-center pt-4">
                                     <input class="step_video_id" value="{{$step->video_id}}" hidden>
-                                    <video src="{{$step->video_file}}" width="100%" style="max-height: 300px; width: auto"></video>
+                                    <video style="max-height: 300px; width: -webkit-fill-available;" controls poster>
+                                        <source src="{{$step->video_file}}" type=video/ogg>
+                                        <source src="{{$step->video_file}}" type=video/mp4>
+                                    </video>
                                 </div>
                             </div>
                         </div>
