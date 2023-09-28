7924ebc831af52ce899715dbf0d48f5618022557
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2023-09-28 23:58:11 +0330

    adding therapists chart for api

--- app/Http/Controllers/App/ActionController.php
+++ app/Http/Controllers/App/ActionController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Step;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Morilog\Jalali\CalendarUtils;
 use Morilog\Jalali\Jalalian;
 
 class ActionController extends Controller
@@ -32,33 +34,66 @@ class ActionController extends Controller
             'month' => ['required', 'numeric', 'min:1', 'max:12']
         ]);
 
-        $content_id = $request->get('content_id');
-        $results = [];
-
         //finding steps
+        $content_id = $request->get('content_id');
         $content = $user->contents()->find($content_id);
         $cluster_id = $content->pivot->cluster_id;
 
+        //get scores with dates--------------------------------------------------------------------------------------
         $month = $request->get('month');
         $last_day = $month < 7 ? 31 : 30;
+
+        $results = [];
+        $visit_results = [];
+        $visit_dates = [];
 
         for($i = 1; $i <= $last_day; $i++){
             $start_date = new Jalalian(Jalalian::now()->getYear(), $month, $i, 0, 0, 0);
             $end_date = new Jalalian(Jalalian::now()->getYear(), $month, $i, 23, 59, 59);
 
             $actions = $user->actions()
-                            ->whereHas('step', function($q) use ($cluster_id) {
-                                $q->where('cluster_id', $cluster_id);
-                            })
-                            ->whereBetween('created_at', [$start_date->toCarbon(), $end_date->toCarbon()])
-                            ->groupBy('step_id')
-                            ->selectRaw('sum(count * result)/sum(count) as avg')
-                            ->get()
-                            ->sum('avg');
+                    ->whereNull('admin_id')
+                    ->whereHas('step', function($q) use ($cluster_id) {
+                        $q->where('cluster_id', $cluster_id);
+                    })
+                    ->whereBetween('created_at', [$start_date->toCarbon(), $end_date->toCarbon()])
+                    ->groupBy('step_id')
+                    ->selectRaw('sum(count * result)/sum(count) as avg')
+                    ->get()
+                    ->sum('avg');
 
             $results[] = $actions;
         }
 
-        return $this->customSuccess($results, "ارزیابی");
+        $visit_actions = $user->actions()
+            ->whereNotNull('admin_id')
+            ->whereHas('step', function($q) use ($cluster_id) {
+                $q->where('cluster_id', $cluster_id);
+            })
+            ->select(DB::raw('DATE(created_at) as date'), DB::raw('step_id as step_id'))
+            ->groupBy('date', 'step_id')
+            ->selectRaw('sum(count * result)/sum(count) as avg')
+            ->get();
+
+        $last_date = null;
+        foreach ($visit_actions as $visit_action){
+            if($visit_action->date == $last_date){
+                $last_visit_score_in_a_day = array_pop($visit_results);
+                $last_visit_score_in_a_day += $visit_action->avg;
+                array_push($visit_results, $last_visit_score_in_a_day);
+            }else{
+                $visit_dates[] = CalendarUtils::strftime('m/d', strtotime($visit_action->date));
+                $last_date = $visit_action->date;
+                array_push($visit_results, $visit_action->avg);;
+            }
+        }
+
+        $data = [
+            'results' => $results,
+            'visit_dates' => $visit_dates,
+            'visit_results' => $visit_results
+        ];
+
+        return $this->customSuccess($data, "ارزیابی");
     }
 }
