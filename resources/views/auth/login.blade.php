0fc0d5c6352e1018b7c3062f2d2cd1592f38f53c
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2024-01-05 13:56:53 +0330

    adding logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,14 +43,15 @@
 <div class="container d-flex align-items-center justify-content-center vh-100">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="row justify-content-center">
-                <a href="{{url('/')}}"  class="col-5">
+            <div class="row justify-content-center p-2">
+{{--                <a href="{{url('/')}}"  class="col-5">--}}
+                    <img src="{{asset('assets/img/logo.png')}}" width="100%" class="col-5">
 {{--                    @include('logo')--}}
 
-                </a>
+{{--                </a>--}}
             </div>
 
-            <div class="card-body p-5">
+            <div class="card-body p-2">
                 <h4 class="text-dark mb-5 text-center">ورود به پنل مدیریت</h4>
 
                 <form method="POST" action="{{ route('login') }}" id="login_form">
